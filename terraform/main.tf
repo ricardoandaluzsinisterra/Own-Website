cd42aff92cc4cb169714ebdb07517c9dc6e43280
Ricardo <code@ricardoandaluz.com>
2026-03-11 18:18:42 +0000

    Update Terraform configuration: correct Azure subscription and tenant IDs, change resource group location to 'westeurope', and remove custom domain resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "38b66f09-c438-476e-bdfc-4005c0a4a5ac"
-  tenant_id       = "3b79a646-3c26-41cf-b144-8eb84dfe71f7"
+  subscription_id = "ca7824a5-1e57-4c4e-87f0-c8aebbbeea88"
+  tenant_id       = "01e413d4-0d2d-48ec-80fc-0b87c981437d"
 }
 
 resource "azurerm_resource_group" "ra_website" {
   name     = "ra-website_group"
-  location = "West US 2"
+  location = "westeurope"
 }
 
 resource "azurerm_static_web_app" "ra_website" {
@@ -28,8 +28,3 @@ resource "azurerm_static_web_app" "ra_website" {
   tags = {}
 }
 
-resource "azurerm_static_web_app_custom_domain" "ra_domain" {
-  static_web_app_id = azurerm_static_web_app.ra_website.id
-  domain_name       = "ricardoandaluz.com"
-  validation_type   = "cname-delegation"
-}
